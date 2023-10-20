fda720999ca10d7c89039e64dc549f2ced1c09ee
Warejali <waraj.com@gmail.com>
2023-10-20 18:44:09 +0600

    first commit

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -1,6 +1,6 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { NextResponse } from "next/server";
 
-export async function post(req: NextApiRequest, res: NextApiResponse) {
-    return NextResponse.json({ name: "File uploaded" });
-}
+// export async function post(req: NextApiRequest, res: NextApiResponse) {
+//     return NextResponse.json({ name: "File uploaded" });
+// }
