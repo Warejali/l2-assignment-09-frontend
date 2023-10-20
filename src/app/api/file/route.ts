1b52ced482c0460d98221e8ea4a44ce8afb8ebd2
Warejali <waraj.com@gmail.com>
2023-10-20 18:47:04 +0600

    first commit

--- src/app/api/file/route.ts
+++
@@ -1,6 +0,0 @@
-// import { NextApiRequest, NextApiResponse } from "next";
-// import { NextResponse } from "next/server";
-
-// export async function post(req: NextApiRequest, res: NextApiResponse) {
-//     return NextResponse.json({ name: "File uploaded" });
-// }
